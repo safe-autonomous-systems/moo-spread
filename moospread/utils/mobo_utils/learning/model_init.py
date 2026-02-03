f7ee4d933c284a449476c8dfb02985579918fbd0
Salomon Hotegni <91737217+salomonhotegni@users.noreply.github.com>
2026-02-03 14:10:21 +0100

    Clean up model_init.py by removing comments
    Removed commented print statements for data shape and class weights.

--- moospread/utils/mobo_utils/learning/model_init.py
+++ moospread/utils/mobo_utils/learning/model_init.py
@@ -4,7 +4,6 @@ from moospread.utils.mobo_utils.learning.model import NeuralNet
 import torch.nn as nn
 
 
-
 def init_dom_nn_classifier(x, y, rel_map, dom, n_var, batch_size=32,
                            activation='relu', lr=0.001, weight_decay=0.00001):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -13,10 +12,7 @@ def init_dom_nn_classifier(x, y, rel_map, dom, n_var, batch_size=32,
     num_hidden_layers = 3
     epochs = 20
     data = prepare_dom_data(x, y, rel_map, dom, data_kind='tensor', device=device)
-    # print(f"Data shape: {data.shape}")
-    # print("data[:, -1]", data[:, -1])
     weight = compute_class_weight(data[:, -1])
-    # print(f"Class weights: {weight}")
     if weight is None:
         return None
 
